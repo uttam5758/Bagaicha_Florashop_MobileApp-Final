846a7c9576c3cc54668e146224b2f1c9da66785b
Uttam Sapkota <uttamsapkota.danger@gmail.com>
2025-07-29 13:28:21 +0545

    Base url + get product url added

--- lib/api/productAPI.dart
+++ lib/api/productAPI.dart
@@ -8,20 +8,19 @@ import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
-
 class ProductAPI {
   Future<ProductResponse?> getproduct(String? search) async {
     Future.delayed(const Duration(seconds: 2), () {});
     ProductResponse? productResponse;
     Response response;
 
-    // try {
-    //   var dio = Dio();
-    //   // dio.interceptors.add(DioCacheManager(CacheConfig(baseUrl: "http://www.localhost:5000")).interceptor);
-    //   var url = baseUrl + getProductsUrl;
-    //   response = await dio.get(url, queryParameters: {'keyword': search}
-    //   // ,options: buildCacheOptions(Duration(days: 7))
-    //   );
+    try {
+      var dio = Dio();
+      // dio.interceptors.add(DioCacheManager(CacheConfig(baseUrl: "http://www.localhost:5000")).interceptor);
+      var url = baseUrl + getProductsUrl;
+      response = await dio.get(url, queryParameters: {'keyword': search}
+          // ,options: buildCacheOptions(Duration(days: 7))
+          );
 
       if (response.statusCode == 200) {
         productResponse = ProductResponse.fromJson(response.data);
